13101cb32073bdbe432396c06b0d4f27a07f0a4b
spreed <1090329705@qq.com>
2018-12-23 17:50:15 +0800

    init framework

---
+++ src/tools/Instance.java
@@ -0,0 +1,134 @@
+package tools;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Instance {
+	private int numOfFacility;
+	private int numOfCustomer;
+	private Facility[] facilities;
+	private int[] demands;
+	private int[][] assignCost;
+	
+	public Instance(String filePath) {
+		Scanner s = null;
+		try {
+			s = new Scanner(new BufferedReader(new FileReader(filePath)));
+			
+			this.numOfFacility = s.nextInt();
+			this.numOfCustomer = s.nextInt();
+			
+			this.facilities = new Facility[numOfFacility];
+			this.demands = new int[numOfCustomer];
+			this.assignCost = new int[numOfFacility][numOfCustomer];
+			
+			for (int i = 0; i < numOfFacility; i++) {
+				int capacity = s.nextInt();
+				int openCost = s.nextInt();
+				facilities[i] = new Facility(capacity, openCost);
+			}
+			
+			for (int i = 0; i < numOfCustomer; i++) {
+				demands[i] = (int) s.nextFloat();
+			}
+			
+			for (int i = 0; i < numOfFacility; i++) {
+				for (int j = 0; j < numOfCustomer; j++) {
+					assignCost[i][j] = (int) s.nextFloat();
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public class Solution {
+		private int[] line;
+		private int cost;
+		private boolean isDirty;
+		
+		 public Solution() {
+			this.line = new int[Instance.this.numOfCustomer];
+			this.isDirty = false;
+			/*TODO something need*/
+		 }
+		 
+		 public boolean isValid() {
+			 int[] capacities = new int[Instance.this.numOfFacility];
+			 
+			 for (int i = 0; i < Instance.this.numOfFacility; i++) capacities[i] = 0;
+			 for (int i = 0; i < line.length; i++) capacities[line[i]] += Instance.this.demands[i];
+			 
+			 for (int i = 0; i < Instance.this.numOfFacility; i++) {
+				 if (capacities[i] > Instance.this.facilities[i].getCapacity()) 
+					 return false;
+			 }
+			 
+			 return true;
+		 }
+		 
+		 public int getCost() {
+			 if (isDirty) {
+				 cost = computeCost();
+			 }
+			 return cost;
+		 }
+		 
+		 private int computeCost() {
+			 
+		 }
+		 
+		 public Solution clone() {
+			 Solution ret = new Solution();
+			 for (int i = 0; i < Instance.this.numOfCustomer; i++) {
+				 ret.line[i] = this.line[i];
+			 }
+			 return ret;
+		 }
+	}
+	
+	private class Facility {
+		private int capacity;
+		private int openCost;
+		
+		public Facility(int _capacity, int _openCost) {
+			this.capacity = _capacity;
+			this.openCost = _openCost;
+		}
+
+		public int getCapacity() {
+			return capacity;
+		}
+
+		public void setCapacity(int capacity) {
+			this.capacity = capacity;
+		}
+
+		public int getOpenCost() {
+			return openCost;
+		}
+
+		public void setOpenCost(int openCost) {
+			this.openCost = openCost;
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		String filePath = "Instances/p1";
+		Scanner s = null;
+		try {
+			s = new Scanner(new BufferedReader(new FileReader(filePath)));
+			while(s.hasNext()) {
+				System.out.println(s.nextFloat());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
